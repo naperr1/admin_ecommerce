ab11ee18415b45d8950945bfa7ee1b55d5749142
Phát <97102848+naperr1@users.noreply.github.com>
2024-07-17 22:52:10 +0700

    update route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react";
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Product from "./pages/Product";
+import User from "./pages/User";
+import Order from "./pages/Order";
+import Catalog from "./pages/Catalog";
+import Voucher from "./pages/Voucher";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h3 className="p-10">Hello</h3>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/voucher" element={<Voucher />} />
+      </Routes>
     </>
   );
 }
