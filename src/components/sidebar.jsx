c6096435ceecc9a3abbf64e08c124b37a1d0e09d
huy20052002 <148831645+huy20052002@users.noreply.github.com>
2024-07-17 22:48:28 +0700

    Update sidebar.jsx

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -2,12 +2,12 @@ export default function Sidebar() {
     return (
         <>
             <div className="">
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
+                <div className="flex items-center gap-2"></div>
+                <div className="flex items-center gap-2"></div>
+                <div className="flex items-center gap-2"></div>
+                <div className="flex items-center gap-2"></div>
+                <div className="flex items-center gap-2"></div>
+                <div className="flex items-center gap-2"></div>
             </div>
         </>
     );
